f8cf3723d2fa2d023d3ca12e6b11c6a1fb470965
gwililgwi <80652061+gwililgwi@users.noreply.github.com>
2021-05-07 06:14:06 +0900

    Add files via upload

---
+++ 3day/forWhile.js
@@ -0,0 +1,25 @@
+// Use Operator : while
+function useW(){
+    let a = 0;
+    while(a < 7){
+        console.log(a);
+        a++;
+    }
+}
+
+// Use Operator : for
+function useF(){
+    for(let a=0; a<7; a++){
+        console.log(a);
+    }
+}
+
+// 인터넷어 돌아다니는 코딩짤구현
+// 기획의도 : 카운트다운후 최종발사
+// 만들어진것 : 전쟁
+function Fuck(){
+    for(let a=10; a>0; a--){
+        console.log(a);
+        console.log('발사!');
+    }
+}
